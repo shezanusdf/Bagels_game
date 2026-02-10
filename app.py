f95375ffd2b3d6782f348eb9ff960749bc9171c9
shezanusdf <shezan.biz@gmail.com>
2026-02-10 11:27:56 +0530

    made minor changes for deployment purposes

--- app.py
+++ app.py
@@ -90,8 +90,8 @@ def index():
         colorize=colorize)
         
 if __name__ == '__main__':
-    app.run(debug = True)
-    
+    app.run(debug=False, host='0.0.0.0', port=10000)
+
     
     
     
